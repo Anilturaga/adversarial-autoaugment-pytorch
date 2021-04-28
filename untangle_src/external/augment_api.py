dc7d7770d7a8168b5387a7d560cf352fd069f20d
AnilTuraga <31271852+Anilturaga@users.noreply.github.com>
2021-04-28 23:28:03 +0530

    untangle api prelim
    Preliminary file structure for untangle compatible api

---
+++ untangle_src/external/augment_api.py
@@ -0,0 +1,10 @@
+"""
+File should reside in external folder, all functions should reside in adversarial_auto_augment
+folder alongside external folder
+
+FUNCTIONS:
+    main function to take model, dataloaders
+    Evaluate takes model, dataloader for test and corrupstion datasets
+    Logs dir and model checkpoints automatically created complying with version2 
+
+"""
